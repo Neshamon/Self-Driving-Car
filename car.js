f9e891eab331935d494260205076ddf2e29a8709
John Matthews <neshamon@protonmail.com>
2022-05-16 18:47:40 -0500

    Add realistic turns to car

--- car.js
+++ car.js
@@ -9,6 +9,7 @@ class Car{
     this.acceleration = 0.2;
     this.maxSpeed = 3;
     this.friction = 0.05;
+    this.angle= 0;
 
     this.controls = new Controls();
   }
@@ -44,19 +45,33 @@ class Car{
       this.speed = 0;
     }
 
-    this.y -= this.speed;
+    if(this.controls.left){
+      this.angle -= 0.03;
+    }
+
+    if(this.controls.right){
+      this.angle += 0.03;
+    }
+
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
 
   draw(ctx){
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
     ctx.beginPath();
     ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
+      -this.width / 2,
+      -this.height / 2,
       this.width,
       this.height
     ); //@dev param1 defines the x center of the car by placing it within the middle of the width
       //@dev param2 defines the y center of the car by placing it within the middle of the height
       //@dev param3 & 4 defines the overall width and height of the car repsectively
     ctx.fill();
+
+    ctx.restore();
   }
 }
