9a9895605159e35cc02b16d1cb5c57d61395f584
John Matthews <neshamon@protonmail.com>
2022-05-19 10:33:13 -0500

    Implement self driving capabilities

--- car.js
+++ car.js
@@ -12,6 +12,8 @@ class Car{
     this.angle= 0;
     this.damaged = false;
 
+    this.useBrain = controlType == "AI";
+
     if(controlType != "DUMMY"){
       this.sensor = new Sensor(this);
       this.brain = new NeuralNetwork(
@@ -36,6 +38,13 @@ class Car{
       // if it is close, the number will be closer to 1 and if far it will be 0
       const outputs = NeuralNetwork.feedForward(offsets, this.brain);
       console.log(outputs);
+
+      if (this.useBrain) {
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.backward = outputs[3];
+      }
     } //@dev This if statement only updates sensors if a given car has a sensor
   }
 
